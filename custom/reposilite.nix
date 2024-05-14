ea51e374d8329f05d1fc01e4df8d7ca8e59425fd
James (Jamalam) <james@jamalam.tech>
2024-05-14 21:27:37 +0100

    fix: src

--- custom/reposilite.nix
+++ custom/reposilite.nix
@@ -1,10 +1,10 @@
-{ stdenv, fetchurl, makeWrapper, openjdk_17_headless }:
+{ stdenv, makeWrapper, openjdk_17_headless }:
 
 stdenv.mkDerivation {
   pname = "reposilite";
   version = "3.5.12";
 
-  src = fetchurl {
+  src = builtins.fetchurl {
     url = "https://maven.reposilite.com/releases/com/reposilite/reposilite/${version}/reposilite-${version}-all.jar";
     sha256 = "7e27421e4768403f68ca7b0fa20ab28405db6f2756da8c5a645c7942778e7126";
   };
@@ -14,7 +14,7 @@ stdenv.mkDerivation {
   installPhase = ''
     runHook preInstall
     makeWrapper ${openjdk_17_headless}/bin/java $out/bin/reposilite \
-      --add-flags "-Xmx40m -jar $jar" \
+      --add-flags "-Xmx40m -jar ${src}" \
       --set JAVA_HOME ${openjdk_17_headless}
     runHook postInstall
   '';
