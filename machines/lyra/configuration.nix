022a6c86a1b4893bb5880572eccd595056fc5878
James (Jamalam) <james@jamalam.tech>
2024-06-17 18:02:15 +0100

    feat: enable sat bot on lyra

--- machines/lyra/configuration.nix
+++ machines/lyra/configuration.nix
@@ -1,6 +1,8 @@
 {
   inputs,
   outputs,
+  pkgs,
+  config,
   ...
 }: {
   imports = [
@@ -10,6 +12,7 @@
     ./disk-configuration.nix
 
     ./../../modules/nixos/base.nix
+    ./../../services/lyra/sat-bot.nix
   ];
 
   boot.loader.grub.enable = true;
@@ -27,9 +30,25 @@
     };
   };
 
-  sops.secrets.lyra-password.neededForUsers = true;
-  sops.secrets.lyra-password = {};
-
   networking.hostName = "lyra";
   networking.networkmanager.enable = true;
+
+  sops.secrets.lyra-password.neededForUsers = true;
+  sops.secrets.lyra-password = {};
+  sops.secrets.sat-bot-discord-token.neededForUsers = true;
+  sops.secrets.sat-bot-discord-token = {};
+  sops.secrets.sat-bot-guild-id.neededForUsers = true;
+  sops.secrets.sat-bot-guild-id = {};
+  sops.secrets.sat-bot-n2yo-key.neededForUsers = true;
+  sops.secrets.sat-bot-n2yo-key = {};
+
+  services.sat-bot = {
+    enable = true;
+    package = pkgs.callPackage ./../../custom/sat-bot.nix {};
+    settings = {
+      discordToken = config.sops.secrets.sat-bot-discord-token.path;
+      guildId = config.sops.secrets.sat-bot-guild-id.path;
+      n2yoKey = config.sops.secrets.sat-bot-n2yo-key.path;
+    };
+  };
 }
