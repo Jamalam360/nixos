0786ad5cbf9a33baaadf4dd4bea97c4d0b7765ff
Jamalam <james@jamalam.tech>
2024-07-02 19:09:55 +0100

    chore: cleanup

--- modules/home-manager/hercules/_packages.nix
+++ modules/home-manager/hercules/_packages.nix
@@ -2,38 +2,38 @@
   home = {
     packages = with pkgs; [
       # == Development ==
-      pkgs.deno
-      pkgs.jetbrains-toolbox
-      pkgs.nil
-      pkgs.sculk
-      pkgs.vscode
-      pkgs.warp-terminal
+      deno
+      jetbrains-toolbox
+      nil
+      sculk
+      vscode
+      warp-terminal
 
       # == Productivity ==
-      pkgs._1password-gui
-      pkgs.google-chrome
-      pkgs.obsidian
-      pkgs.okular
-      pkgs.yubioath-flutter
+      _1password-gui
+      google-chrome
+      obsidian
+      okular
+      yubioath-flutter
 
       # == Social ==
-      pkgs.discord
-      pkgs.spotify
+      discord
+      spotify
 
       # == Gaming ==
-      pkgs.obs-studio
-      pkgs.prismlauncher
-      pkgs.steam
+      obs-studio
+      prismlauncher
+      steam
 
       # == Maker ==
-      pkgs.candle
-      pkgs.flashprint
+      candle
+      flashprint
 
       # == SDR ==
-      pkgs.audacity
-      pkgs.gpredict
-      pkgs.noaa-apt
-      pkgs.sdrpp
+      audacity
+      gpredict
+      noaa-apt
+      sdrpp
     ];
   };
 }
