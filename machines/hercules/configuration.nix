418761fe2d0faaee14db7effb3ddac328b701345
Jamalam <james@jamalam.tech>
2024-07-19 18:45:39 +0100

    chore: enable yubikey locking

--- machines/hercules/configuration.nix
+++ machines/hercules/configuration.nix
@@ -64,14 +64,14 @@
   services.pcscd.enable = true;
 
   # Uncomment this to lock the screen when the YubiKey is removed
-  # services.udev.extraRules = ''
-  #   ACTION=="remove",\
-  #   ENV{ID_BUS}=="usb",\
-  #   ENV{ID_MODEL_ID}=="0407",\
-  #   ENV{ID_VENDOR_ID}=="1050",\
-  #   ENV{ID_VENDOR}=="Yubico",\
-  #   RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
-  # '';
+  services.udev.extraRules = ''
+    ACTION=="remove",\
+    ENV{ID_BUS}=="usb",\
+    ENV{ID_MODEL_ID}=="0407",\
+    ENV{ID_VENDOR_ID}=="1050",\
+    ENV{ID_VENDOR}=="Yubico",\
+    RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+  '';
 
   # == Flatpak ==
   services.flatpak.enable = true;
