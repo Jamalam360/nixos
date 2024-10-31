843552977d2849fbc50d0d4526495fe16390a94d
Jamalam <james@jamalam.tech>
2024-10-31 11:03:31 +0000

    feat: use systemd timers for nix builder

--- services/lyra/nix-builder.nix
+++ services/lyra/nix-builder.nix
@@ -4,21 +4,34 @@
 }: let
   buildSystems = pkgs.writeShellScript "build-systems.sh" ''
     dir="/tmp/system-build-$(date -Iseconds)"
+    mkdir dir
     git clone https://github.com/Jamalam360/nixos "$dir"
     cd "$dir"
 
     echo "Building Hercules"
-    nixos-rebuild build --accept-flake-config --flake .#hercules
+    nix-shell -p '(nixos{}).nixos-rebuild' --command "nixos-rebuild build --accept-flake-config --flake .#hercules"
     echo "Building Leo"
-    nixos-rebuild build --accept-flake-config --flake .#leo
+    nix-shell -p '(nixos{}).nixos-rebuild' --command "nixos-rebuild build --accept-flake-config --flake .#leo"
     echo "Building Lyra"
-    nixos-rebuild build --accept-flake-config --flake .#lyra
+    nix-shell -p '(nixos{}).nixos-rebuild' --command "nixos-rebuild build --accept-flake-config --flake .#lyra"
   '';
 in {
-  services.cron = {
-    enable = true;
-    systemCronJobs = [
-      "0 7 * * *  root  ${buildSystems}"
-    ];
+  systemd.timers."nix-builder" = {
+    wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "07:00";
+        Persistent = true;
+        Unit = "nix-builder.service";
+      };
+  };
+
+  systemd.services."nix-builder" = {
+    script = ''
+      ${buildSystems}
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
   };
 }
