5328d07a6c047ba13ca9408191a031474cf35100
Jamalam <james@jamalam.tech>
2024-12-03 00:10:04 +0000

    feat: vscode config

--- modules/gnome/vscode.nix
+++ modules/gnome/vscode.nix
@@ -1,6 +1,65 @@
 {pkgs, ...}: {
   programs.vscode = {
     enable = true;
+    userSettings = {
+      "[astro]"."editor.defaultFormatter" = "astro-build.astro-vscode";
+      "[c]"."editor.defaultFormatter" = "ms-vscode.cpptools";
+      "[go]"."editor.defaultFormatter" = "golang.go";
+      "[html]"."editor.defaultFormatter" = "vscode.html-language-features";
+      "[jsonc]"."editor.defaultFormatter" = "vscode.json-language-features";
+      "[nix]"."editor.defaultFormatter" = "jnoortheen.nix-ide";
+      "[python]"."editor.defaultFormatter" = "ms-python.black-formatter";
+      "[rust]"."editor.defaultFormatter" = "rust-lang.rust-analyzer";
+      "[svelte]"."editor.defaultFormatter" = "svelte.svelte-vscode";
+      "[toml]"."editor.defaultFormatter" = "tamasfe.even-better-toml";
+      "[typescript]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+      "[xml]"."editor.defaultFormatter" = "redhat.vscode-xml";
+      "[yaml]"."editor.defaultFormatter" = "redhat.vscode-yaml";
+
+      "editor.defaultFormatter" = "esbenp.prettier-vscode";
+      "editor.detectIndentation" = true;
+      "editor.fontFamily" = "'Inconsolata', 'monospace', monospace";
+      "editor.fontSize" = 16;
+      "editor.formatOnSave" = true;
+      "editor.formatOnType" = true;
+      "editor.inlineSuggest.enable" = true;
+      "editor.insertSpaces" = false;
+      "editor.minimap.enabled" = false;
+      "editor.renderWhitespace" = "trailing";
+      "editor.stickScroll.maxLineCount" = 3;
+      "editor.stickyScroll.enabled" = true;
+      "editor.tabSize" = 4;
+      "explorer.confirmDragAndDrop" = false;
+      "files.autoSave" = "afterDelay";
+      "files.autoSaveDelay" = 1000;
+      "git.autoFetch" = true;
+      "git.confirmSync" = false;
+      "git.enableSmartCommit" = true;
+      "git.openRepositoryInParentFolders" = "never";
+      "github.copilot.editor.enableAutoCompletions" = true;
+      "javascript.updateImportsOnFileMove.enabled" = "always";
+      "prettier.printWidth" = 120;
+      "redhat.telemetry.enabled" = false;
+      "search.followSymlinks" = false;
+      "typescript.updateImportsOnFileMove.enabled" = "always";
+      "vsicons.dontShowNewVersionMessage" = true;
+      "window.autoDetectColorScheme" = true;
+      "workbench.preferredDarkColorTheme" = "Solarized Dark";
+      "workbench.preferredLightColorTheme" = "Solarized Light";
+      "[markdown]" = {
+        "editor.defaultFormatter" = "edbenp.prettier-vscode";
+        "editor.wordWrap" = "on";
+        "editor.quickSuggestions" = {
+          comments = "off";
+          other = "off";
+          strings = "off";
+        };
+        "editor.unicodeHighlight" = {
+          ambiguousCharacters = false;
+          invisibleCharacters = false;
+        };
+      };
+    };
     extensions = with pkgs.vscode-marketplace; [
       astro-build.astro-vscode
       barbosshack.crates-io
