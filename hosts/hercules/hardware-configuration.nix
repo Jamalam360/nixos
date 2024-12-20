f766042cc7def3321ab05de9ab5f23418b71d402
Jamalam <james@jamalam.tech>
2024-12-20 21:57:04 +0000

    feat: set hercules cpu governor to performance

--- hosts/hercules/hardware-configuration.nix
+++ hosts/hercules/hardware-configuration.nix
@@ -30,4 +30,5 @@
   hardware.bluetooth.enable = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  powerManagement.cpuFreqGovernor = "performance";
 }
