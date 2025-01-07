87f19ae40f97efe30de9e722a360bf3e363da487
Jamalam <james@jamalam.tech>
2025-01-07 16:55:57 +0000

    fix: switch to beta nvidia drivers

--- modules/nvidia/default.nix
+++ modules/nvidia/default.nix
@@ -9,7 +9,7 @@
       powerManagement.finegrained = false;
       open = false;
       nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
     };
   };
 }
