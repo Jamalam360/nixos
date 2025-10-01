0c42816f6b5782f92bfabccef3f7515dc40a9d1e
Jamalam <james@jamalam.tech>
2025-10-01 14:29:36 +0100

    chore: remove old lyra services

--- modules/lyra-services/default.nix
+++ modules/lyra-services/default.nix
@@ -1,16 +1,12 @@
 {...}: {
   imports = [
-    ./cdn.nix
     ./discord-github-releases.nix
     ./gitlab-runner.nix
-    ./its-clearing-up.nix
-    # ./minecraft.nix
     ./nginx.nix
     ./nix-cache.nix
     ./pinguino-quotes.nix
     ./reposilite.nix
     ./restic.nix
     ./sat-bot.nix
-    ./teach-man-fish.nix
   ];
 }
--- modules/lyra-services/modpacks/vanilla.nix
+++
@@ -1,4 +0,0 @@
-{
-  url = "https://raw.githubusercontent.com/Jamalam360/vanilla-server/88e198103812722496c9719e0851f9b4f48821a6";
-  hash = "sha256-KJsx+U0OACKxOqd/p+181PlA2JZcVc3si+8UesbUPwQ=";
-}
