11607418cafab68a64f98f783d6247a2bb36d435
Jamalam <james@jamalam.tech>
2025-10-10 13:33:33 +0100

    fix: remove qemu.ovmf options

--- modules/virtualisation/default.nix
+++ modules/virtualisation/default.nix
@@ -6,8 +6,6 @@
       qemu = {
         package = pkgs.qemu_kvm;
         swtpm.enable = true;
-        ovmf.enable = true;
-        ovmf.packages = [pkgs.OVMFFull.fd];
       };
     };
     spiceUSBRedirection.enable = true;
