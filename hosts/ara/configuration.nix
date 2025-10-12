af4e0609b9a2cb4324e1700e97724d3c42367b23
Jamalam <james@jamalam.tech>
2025-10-12 14:48:58 +0100

    feat: init ara

---
+++ hosts/ara/configuration.nix
@@ -0,0 +1,37 @@
+{
+  inputs,
+  outputs,
+  ...
+}: let
+  root = ../..;
+  nixos-modules = [
+    /.${root}/hosts/base.nix
+    ./hardware-configuration.nix
+  ];
+  home-manager-modules = [
+    /.${root}/hosts/home_base.nix
+  ];
+in {
+  imports = nixos-modules;
+
+  users.users.root.hashedPassword = "!"; # Disable root login
+
+  home-manager = {
+    extraSpecialArgs = {inherit inputs outputs;};
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users = {
+      james = {
+        imports = home-manager-modules;
+      };
+    };
+  };
+
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+  };
+  networking.hostName = "ara";
+  time.timeZone = "Europe/Helsinki";
+  sops.secrets.lyra-password.neededForUsers = true;
+}
