246b242b9fdb42b89959a4082fa0332f28ff32de
Jamalam <james@jamalam.tech>
2025-10-12 15:49:13 +0100

    fix: ensure systems can still build when substituters are down

--- hosts/base.nix
+++ hosts/base.nix
@@ -49,6 +49,11 @@ in {
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
+
+    extraOptions = ''
+      # Ensure systems can still build when substituters are down
+      fallback = true
+    '';
   };
 
   i18n = {
