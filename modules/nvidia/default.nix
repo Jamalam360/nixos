6e2419dd78692699c20980dc541a7470ec240cdf
Jamalam <james@jamalam.tech>
2025-12-09 17:23:06 +0000

    feat(nvidia): switch to stable drivers

--- modules/nvidia/default.nix
+++ modules/nvidia/default.nix
@@ -11,7 +11,7 @@
       powerManagement.finegrained = false;
       open = false;
       nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.beta;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
   };
 }
