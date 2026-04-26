b19a781aa89e9c71c9db77a751f2d542fe0ae9e4
Jamalam <james@jamalam.tech>
2026-04-26 20:39:27 +0100

    feat: java 25

--- modules/jdk/home.nix
+++ modules/jdk/home.nix
@@ -1,11 +1,11 @@
 # inspo: https://discourse.nixos.org/t/fix-collision-with-multiple-jdks/10812/5
 {pkgs, ...}: let
-  additionalJDKs = with pkgs; [temurin-bin-17];
+  additionalJDKs = with pkgs; [temurin-bin-17, temurin-bin-25];
 in {
   programs.java = {
     enable = true;
-    # This determines JAVA_HOME - set to Java 21 at the moment as that is what modern Minecraft uses
-    package = pkgs.temurin-bin-21;
+    # This determines JAVA_HOME - set to Java 25 at the moment as that is what modern Minecraft uses
+    package = pkgs.temurin-bin-25;
   };
 
   home.sessionPath = ["$HOME/.jdks"];
