c270f3a29f6052e93247ccbfeee7f7d13a8efb72
Jamalam <james@jamalam.tech>
2026-07-20 20:47:27 +0100

    fix: switch to adwaita font

--- modules/gnome/stylix.nix
+++ modules/gnome/stylix.nix
@@ -5,26 +5,28 @@
 }: {
   imports = [inputs.stylix.nixosModules.stylix];
 
-  stylix.enable = true;
-  stylix.fonts = {
-    serif = {
-      package = pkgs.cantarell-fonts;
-      name = "Cantarell";
-    };
+  stylix = {
+    enable = true;
+    fonts = {
+      serif = {
+        package = pkgs.adwaita-fonts;
+        name = "Adwaita";
+      };
 
-    sansSerif = {
-      package = pkgs.cantarell-fonts;
-      name = "Cantarell";
-    };
+      sansSerif = {
+        package = pkgs.adwaita-fonts;
+        name = "Adwaita Sans";
+      };
 
-    monospace = {
-      package = pkgs.nerd-fonts.jetbrains-mono;
-      name = "JetBrainsMono Nerd Font";
-    };
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrainsMono Nerd Font";
+      };
 
-    emoji = {
-      package = pkgs.twitter-color-emoji;
-      name = "Twitter Color Emoji";
+      emoji = {
+        package = pkgs.twitter-color-emoji;
+        name = "Twitter Color Emoji";
+      };
     };
   };
 }
